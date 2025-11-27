5d7331f2bf061d2f417d92444b0b4755cf6c32c2
Emeraldtheporcupine <125104247+Emeraldtheporcupine@users.noreply.github.com>
2025-11-26 23:34:02 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+namespace SpriteKind {
+    export const Animation = SpriteKind.create()
+}
+let mySprite = sprites.create(assets.image`blank`, SpriteKind.Animation)
+mySprite.setScale(4, ScaleAnchor.Middle)
+mySprite.setPosition(47, 42)
+music.play(music.createSong(assets.song`The Song Intro`), music.PlaybackMode.UntilDone)
+music.play(music.createSong(assets.song`The Song`), music.PlaybackMode.LoopingInBackground)
+animation.runImageAnimation(
+mySprite,
+assets.animation`Makecode Promo`,
+100,
+false
+)
+timer.after(10000, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Gelbs Promo`,
+    100,
+    false
+    )
+})
